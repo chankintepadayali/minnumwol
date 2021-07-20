30036d214c9bdd5f480254391f8ba7ed3e567def
(hunk!nd3_p4d4y41! <68646925+chunkindepadayali@users.noreply.github.com>
2021-07-20 19:01:52 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -25,6 +25,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC 
     if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
 
     if (match[1] !== '') {
+        let id = match[1];let id = match[1];
         grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
@@ -51,6 +52,7 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC 
     else if (message.reply_message && match[1] == '') {
         grup = await message.client.groupMetadata(message.jid);
         var jids = [];
+        let id = match[1];
         mesaj = '';
         grup['participants'].map(
             async (uye) => {
