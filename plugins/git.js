021e2a3eee6a9d408883c3abae80a6d8436b423f
chunkindepadayali <68646925+chunkindepadayali@users.noreply.github.com>
2021-09-06 02:04:03 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "http://imgur.com/pauOJEi.jpg";
+  r_text[0] = "https://i.imgur.com/2nYs8dN.jpg";
     
     
     var i = Math.floor(1*Math.random())
